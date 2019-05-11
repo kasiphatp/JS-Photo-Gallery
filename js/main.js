4cece962567502424f9879bb2f93d604d96589c8
Kasiphat <kasiphatjp@gmail.com>
2019-05-12 01:46:48 +0700

    Implemented lightbox and search functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Light box
+// Lightbox
 lightbox.option({
   'wrapAround': true,
   'disableScrolling': true,
@@ -6,3 +6,17 @@ lightbox.option({
   'maxWidth': 889,
   'positionFromTop': 80
 })
+
+// Search
+$('#search').on('keyup', function() {
+  const $search_input = $(this).val().toLowerCase();
+
+  $('a[data-title]').each(function() {
+    const $caption = $(this).attr('data-title').toLowerCase();
+    if ($caption.includes($search_input)) {
+      $(this).parent().fadeIn();
+    } else {
+      $(this).parent().fadeOut();
+    }
+  })
+});
